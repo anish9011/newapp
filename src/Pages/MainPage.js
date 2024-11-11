1a00576ea39eac9bc6d334819c3400ff3ce8dd12
anish shetty <anishshetty901@gmail.com>
2024-11-11 21:56:05 +0530

    Added brandnew component into the brand new branch

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Entry from '../Components/Entry'
+import Brandnew from '../Components/Brandnew'
 
 export default function mainPage() {
   return (
     <>
         <Entry/>
+        <Brandnew/>
     </>
   )
 }
