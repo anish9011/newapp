18cdede93f2a627b0417852f2a0bb7c4fecda735
anish shetty <anishshetty901@gmail.com>
2024-11-11 22:04:41 +0530

    sdsad

--- src/Components/Brandnew.js
+++ src/Components/Brandnew.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Brandnew() {
   return (
     <div>
-      <h1 className="text-3xl text-blue-700">this is brand new branch</h1>
+      <h1 className="text-3xl text-blue-700">this is brdasdnd new branch</h1>
     </div>
   )
 }
